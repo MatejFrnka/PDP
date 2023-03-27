855bd5a84f0b864641cfea7401f0a34a79dcb9c7
Matěj Frnka <mat.frnka@gmail.com>
2023-03-27 19:46:01 +0200

    checkpoint_4_0

--- solution.cpp
+++ solution.cpp
@@ -7,11 +7,19 @@
 #include <fstream>
 #include <algorithm>
 #include <chrono>
-#include <omp.h>
 #include <cstring>
+#include <mpi.h>
 
 #define color char
 #define SEQUENTIAL_LIMIT 10
+#define NODE_COUNT 150
+#define EDGE_COUNT 4219
+
+#define TAG_TASK_EDGES 0
+#define TAG_DONE 1
+#define TAG_STOP 2
+#define TAG_TASK_INIT 0
+
 
 using namespace std;
 static const color NO_COLOR = 0;
@@ -20,7 +28,12 @@ static const color B_COLOR = -1;
 
 
 int maxWeightAchieved = 0;
-color *bestConfiguration = new color[0];
+color bestConfiguration[NODE_COUNT];
+
+struct Outcome {
+    int maxWeight;
+    color colorsConfiguration[NODE_COUNT];
+};
 
 struct Edge {
     int a;
@@ -33,18 +46,22 @@ struct Edge {
 };
 
 struct State {
-    Edge *edges;
     int edgesLen;
-    color *colors;
     int colorsLen;
     int weight;
     int i;
     int remainingWeight;
+    color colors[NODE_COUNT];
+    Edge *edges;
+
+    State() = default;
 
     State(Edge *edges, int edgesLen, color *colors, int colorsLen, int weight, int i, int remainingWeight) :
-            edges(edges), edgesLen(edgesLen), colors(colors), colorsLen(colorsLen), weight(weight), i(i),
+            edges(edges), edgesLen(edgesLen), colorsLen(colorsLen), weight(weight), i(i),
             remainingWeight(remainingWeight) {
-
+        for (int j = 0; j < colorsLen; ++j) {
+            this->colors[j] = colors[j];
+        }
     }
 };
 
@@ -60,7 +77,6 @@ void handleEdge(const Edge *edges, int edgesLen, color *colors, int colorsLen, i
     // save colors
     color colorA = colors[nodeAId];
     color colorB = colors[nodeBId];
-
     // set color A if not set already
     if (colors[nodeAId] == NO_COLOR) {
         colors[nodeAId] = A_COLOR;
@@ -122,14 +138,8 @@ void maximumBipartite(const Edge *edges, int edgesLen, color *colors, int colors
                       int remainingWeight) {
     if (i == edgesLen) {
         if (weight > maxWeightAchieved) {
-            #pragma omp critical
             {
-                // check again in critical section
-                if (weight > maxWeightAchieved) {
-                    maxWeightAchieved = weight;
-                }
-                delete[] bestConfiguration;
-                bestConfiguration = new color[colorsLen];
+                maxWeightAchieved = weight;
                 for (int j = 0; j < colorsLen; ++j) {
                     bestConfiguration[j] = colors[j];
                 }
@@ -149,70 +159,129 @@ void maximumBipartite(const Edge *edges, int edgesLen, color *colors, int colors
 
 }
 
+
+void handleIncomingMessage(Outcome &maximum, MPI_Status &status) {
+    Outcome outcome{};
+    MPI_Recv(&outcome, sizeof(Outcome), MPI_CHAR, MPI_ANY_SOURCE, TAG_DONE, MPI_COMM_WORLD, &status);
+    if (outcome.maxWeight > maximum.maxWeight) {
+        maximum = outcome;
+    }
+}
+
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        std::cout << "Required 1 parameter with path to input file" << std::endl;
-        exit(1);
-    }
-    char *filename = argv[1];
+    MPI_Init(&argc, &argv);
 
-    ifstream inputFile(filename);
+    int processRank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &processRank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    string line;
-    if (!getline(inputFile, line)) {
-        std::cout << "Invalid file" << std::endl;
-        exit(1);
-    }
-    int nodeCount = stoi(line);
+    if (processRank == 0) {
+        if (argc != 2) {
+            std::cout << "Required 1 parameter with path to input file" << std::endl;
+            exit(1);
+        }
+        char *filename = argv[1];
 
-    Edge *edges = new Edge[nodeCount * nodeCount];
-    int edgesLen = 0;
-    int maxWeight = 0;
-    for (int i = 0; i < nodeCount; ++i) {
-        for (int j = 0; j < nodeCount; ++j) {
-            int weight;
-            inputFile >> weight;
-            if (j < i) {
-                // skip edges going in the other direction
-                continue;
+        ifstream inputFile(filename);
+
+        string line;
+        if (!getline(inputFile, line)) {
+            std::cout << "Invalid file" << std::endl;
+            exit(1);
+        }
+        int nodeCount = stoi(line);
+
+        Edge *edges = new Edge[nodeCount * nodeCount];
+        int edgesLen = 0;
+        int maxWeight = 0;
+        for (int i = 0; i < nodeCount; ++i) {
+            for (int j = 0; j < nodeCount; ++j) {
+                int weight;
+                inputFile >> weight;
+                if (j < i) {
+                    // skip edges going in the other direction
+                    continue;
+                }
+                maxWeight += weight;
+                if (weight != 0) {
+                    edges[edgesLen++] = Edge(i, j, weight);
+                }
             }
-            maxWeight += weight;
-            if (weight != 0) {
-                edges[edgesLen++] = Edge(i, j, weight);
+        }
+
+        // sort edges descending
+        sort(edges, edges + edgesLen, [](Edge &a, Edge &b) { return a.weight > b.weight; });
+
+        auto *colors = new color[nodeCount];
+        for (int i = 0; i < nodeCount; ++i) {
+            colors[i] = NO_COLOR;
+        }
+
+        auto start = chrono::high_resolution_clock::now();
+        auto states = vector<State>();
+        maximumBipartiteInit(edges, edgesLen, colors, nodeCount, 0, 0, maxWeight, states);
+
+        Outcome maximum{maxWeight = 0};
+
+        for (int i = 1; i <= states.size(); ++i) {
+            char *stateBuffer = (char *) &states[i];
+            int target = i;
+            if (i >= size) {
+                MPI_Status status;
+                handleIncomingMessage(maximum, status);
+                target = status.MPI_SOURCE;
             }
+
+            int edgesSize = static_cast<int>(sizeof(Edge)) * states[i].edgesLen;
+
+            MPI_Send(stateBuffer, sizeof(State), MPI_CHAR, target, TAG_TASK_INIT, MPI_COMM_WORLD);
+            MPI_Send(states[i].edges, edgesSize, MPI_CHAR, target, TAG_TASK_EDGES, MPI_COMM_WORLD);
+        }
+        // finish
+        for (int i = 1; i < size; ++i) {
+            MPI_Status status;
+            handleIncomingMessage(maximum, status);
+            int tmp = 0;
+            MPI_Send(&tmp, 1, MPI_INT, status.MPI_SOURCE, TAG_STOP, MPI_COMM_WORLD);
+        }
+
+        cout << "evalution time: "
+             << chrono::duration_cast<std::chrono::milliseconds>(chrono::high_resolution_clock::now() - start).count()
+             << "ms" << endl;
+
+        cout << "result: " << maximum.maxWeight << endl;
+        for (int i = 0; i < nodeCount; ++i) {
+            cout << (int) maximum.colorsConfiguration[i] << ", ";
+        }
+        cout << endl;
+
+    } else {
+        while (true) {
+            MPI_Status status;
+            State state{};
+            MPI_Recv(&state, sizeof(State), MPI_CHAR, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+
+            if (status.MPI_TAG == TAG_STOP) {
+                break;
+            }
+
+            int bufferSize = static_cast<int>(sizeof(Edge)) * state.edgesLen;
+            char *buffer = new char[bufferSize];
+            MPI_Recv(buffer, bufferSize, MPI_CHAR, 0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+            state.edges = (Edge *) buffer;
+            maximumBipartite(state.edges, state.edgesLen, state.colors, state.colorsLen,
+                             state.weight, state.i, state.remainingWeight);
+
+            Outcome outcome{maxWeightAchieved};
+            for (int i = 0; i < state.colorsLen; ++i) {
+                outcome.colorsConfiguration[i] = bestConfiguration[i];
+            }
+
+            MPI_Send(&outcome, sizeof(Outcome), MPI_CHAR, 0, TAG_DONE, MPI_COMM_WORLD);
         }
     }
 
-    // sort edges descending
-    sort(edges, edges + edgesLen, [](Edge &a, Edge &b) { return a.weight > b.weight; });
-
-    auto *colors = new color[nodeCount];
-    for (int i = 0; i < nodeCount; ++i) {
-        colors[i] = NO_COLOR;
-    }
-
-    auto start = chrono::high_resolution_clock::now();
-    auto states = vector<State>();
-
-    maximumBipartiteInit(edges, edgesLen, colors, nodeCount, 0, 0, maxWeight, states);
-    cout << "size: " << states.size() << endl;
-
-    #pragma omp parallel for schedule(dynamic) default(none) shared(states)
-    for (int i = 0; i < states.size(); ++i) {
-        maximumBipartite(states[i].edges, states[i].edgesLen, states[i].colors, states[i].colorsLen, states[i].weight,
-                         states[i].i, states[i].remainingWeight);
-    }
-
-    cout << "evalution time: "
-         << chrono::duration_cast<std::chrono::milliseconds>(chrono::high_resolution_clock::now() - start).count()
-         << "ms" << endl;
-
-    delete[] edges;
-    delete[] colors;
-    for (int i = 0; i < nodeCount; ++i) {
-        cout << i << " - " << (bestConfiguration[i] == A_COLOR) << endl;
-    }
-    delete[] bestConfiguration;
-    cout << maxWeightAchieved << endl;
+    MPI_Finalize();
     return 0;
 }
